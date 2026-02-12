61d57ef68d9d23241b0adea83e441457e33da3bc
Sultan Mahmud Rana <gsmrana@gmail.com>
2026-02-12 15:33:00 +0600

    Email service unit test updated

--- tests/test_email.py
+++ tests/test_email.py
@@ -4,31 +4,25 @@ from pathlib import Path
 
 # Add project root to Python path
 sys.path.append(str(Path(__file__).parent.parent))
-from app.core.email import send_email, send_email_template
+from app.core.email import email_service
 
 RECIPIENT_EMAILS = ["user@example.com"]
 
-@pytest.mark.asyncio
-async def test_send_email():
-    await send_email(
-        recipients=RECIPIENT_EMAILS,
-        subject="FastAPI Email Test",
-        body="<h3>Wecome to FastAPI Email Service</h3>"
-    )
-    assert True
 
 @pytest.mark.asyncio
-async def test_send_email_template():
-    template_body = {
+async def test_send_email():
+    subject = "Test Email"
+    body = {
         "username": "User1",
         "app_name": "FastAPI App",
         "year": 2026,
         "verify_url": "https://yourdomain.com/verify_url",
         "support_url": "https://yourdomain.com/support",
     }    
-    await send_email_template(
+    result = await email_service.send_email(
         recipients=RECIPIENT_EMAILS,
-        subject="FastAPI Email Test",
-        template_body=template_body
+        subject=subject,
+        template_body=body,
+        template_name="user_welcome.html"
     )
-    assert True
+    assert result is not None
